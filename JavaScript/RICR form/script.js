d32370048bf663fa5c147f86b70f826bc21b880d
sanjana-vi20 <vishwakarmasunita963@gmail.com>
2025-11-19 18:52:21 +0530

    Refactor form submission function and enhance input handling in index.html and script.js

--- JavaScript/RICR form/script.js
+++ JavaScript/RICR form/script.js
@@ -1,13 +1,24 @@
-function submit()
+function submitt()
 {
 
-    const name = document.getElementById("person").value;
-    const contact = document.getElementById("contact").value;
-    const email = document.getElementById("email").value;
-    const qualification = document.getElementById("Qualification").value;
-    const collegeName = document.getElementById("collegeName").value;
-    const year = document.getElementById("year").value;
-    const branch = document.getElementById("branch").value;
-    const course = document.getElementById("course").value;
-    const source = document.getElementById("source").value;
+    const name = document.getElementById("person").value || "";
+    const contact = document.getElementById("contact").value || "";
+    const email = document.getElementById("email").value || "";
+    const qualification = document.getElementById("Qualification").value || "";
+    const collegeName = document.getElementById("collegeName").value || "";
+    const year = document.getElementById("year").value || "";
+    const branch = document.getElementById("branch").value || "";
+    const course = document.getElementById("course").value || "";
+    const source = document.getElementById("source").value || "";
+
+    console.log(name);
+    console.log(contact);
+    console.log(email);
+    console.log(qualification);
+    console.log(collegeName);
+    console.log(year);
+    console.log(branch);
+    console.log(course);
+    console.log(source);
+    alert("Form Submitted Successfully!");
 }
