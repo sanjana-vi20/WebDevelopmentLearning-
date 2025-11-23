d43ff411a2ea4f9837a33eded35d9d193fd296ea
sanjana-vi20 <vishwakarmasunita963@gmail.com>
2025-11-23 14:52:01 +0530

    Add form submission functionality and enhance input handling in script.js and index.html

---
+++ JavaScript/Registration/script.js
@@ -0,0 +1,31 @@
+function submit() {
+
+
+  const fullName = document.getElementById("fullName").value;
+  const email = document.getElementById("email").value;
+  const contact = document.getElementById("contact").value;
+  const dob = document.getElementById("dob").value;
+  const qualification = document.getElementById("qualification").value;
+  const grade = document.getElementById("Grade").value;
+  const address = document.getElementById("address").value;
+  const city = document.getElementById("city").value;
+  const pinCode = document.getElementById("PinCode").value;
+  const guardianName = document.getElementById("Gname").value;
+  const guardianContact = document.getElementById("Gnumber").value;
+  const source = document.getElementById("source").value;
+
+  console.log(fullName);
+  console.log(email);
+  console.log(contact);
+  console.log(dob);
+  console.log(qualification);
+  console.log(grade);
+  console.log(address);
+  console.log(city);
+  console.log(pinCode);
+  console.log(guardianName);
+  console.log(guardianContact);
+  console.log(source);
+  
+
+}
