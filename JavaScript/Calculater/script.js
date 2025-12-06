985f9253fb150466c30433b25575da7aae96c626
sanjana-vi20 <vishwakarmasunita963@gmail.com>
2025-12-06 22:19:17 +0530

    Disable calculator display input and add keyboard support for number and operator inputs

--- JavaScript/Calculater/script.js
+++ JavaScript/Calculater/script.js
@@ -18,3 +18,35 @@ function input(char) {
     document.getElementById("display").value = exp;
   }
 }
+
+document.addEventListener("keydown" ,(a) => {
+  if(a.key == "Enter")
+  {
+    input("=");
+  }
+  else if(
+    a.key == "1"||
+    a.key =="2"||
+    a.key =="3"||
+    a.key == "4"||
+    a.key =="5"||
+    a.key =="6"||
+    a.key =="7"||
+    a.key =="8"||
+    a.key =="9"||
+    a.key =="0"||
+    a.key =="+"||
+    a.key =="-"||
+    a.key =="."||
+    a.key =="*"||
+    a.key =="/"
+  )
+  {
+    input(a.key);
+  }
+  else if(a.key === "Backspace")
+  {
+    input("C");
+  }
+  
+})
--- JavaScript/Events/script.js
+++ JavaScript/Events/script.js
@@ -17,8 +17,8 @@ function green() {
   document.getElementById("bulb").style.backgroundColor = "green";
 }
 
-let userColor = document.getElementById("color");
-userColor.addEventListener("change" ,() => changeColor(userColor.value))
+// let userColor = document.getElementById("color");
+// userColor.addEventListener("change" ,() => changeColor(userColor.value))
 
 function changeColor(color){
     document.getElementById("bulb").style.backgroundColor = color;
@@ -39,26 +39,58 @@ function SB_Control()
     }
 }
 
-let Bgcolor = document.getElementById('Bgcolor');
-Bgcolor.addEventListener("change" , ()=> changeBg(Bgcolor.value))
+// let Bgcolor = document.getElementById('Bgcolor');
+// Bgcolor.addEventListener("change" , ()=> changeBg(Bgcolor.value))
 
-function changeBg(color) 
+// function changeBg(color) 
+// {
+//     document.getElementById('text').style.backgroundColor = color;
+// }
+
+// let hColor = document.getElementById('headColor');
+// hColor.addEventListener("change" ,()=> changeHead(hColor.value));
+
+// function changeHead(color)
+// {
+//         document.getElementById('Heading').style.color = color;
+// }
+
+// let pColor = document.getElementById('paraColor');
+// pColor.addEventListener("change" ,()=> changePara(pColor.value))
+
+// function changePara(color)
+// {
+//     document.getElementById('para').style.color = color;
+// }
+
+document.getElementById("c1").addEventListener("mouseover" , () => {
+  fillcolor("red");
+})
+
+document.getElementById("c1").addEventListener("mouseleave", () => {
+  fillcolor("white");
+});
+
+document.getElementById("c2").addEventListener("mouseover" , () => {
+  fillcolor("green");
+})
+document.getElementById("c3").addEventListener("mouseover" , () => {
+  fillcolor("blue");
+})
+document.getElementById("c4").addEventListener("mouseover" , () => {
+  fillcolor("yellow");
+})
+document.getElementById("c5").addEventListener("mouseover" , () => {
+  fillcolor("orange");
+})
+document.getElementById("c6").addEventListener("mouseover" , () => {
+  fillcolor("violet");
+})
+document.getElementById("c7").addEventListener("mouseover" , () => {
+  fillcolor("indigo");
+})
+
+function fillcolor(color)
 {
-    document.getElementById('text').style.backgroundColor = color;
-}
-
-let hColor = document.getElementById('headColor');
-hColor.addEventListener("change" ,()=> changeHead(hColor.value));
-
-function changeHead(color)
-{
-        document.getElementById('Heading').style.color = color;
-}
-
-let pColor = document.getElementById('paraColor');
-pColor.addEventListener("change" ,()=> changePara(pColor.value))
-
-function changePara(color)
-{
-    document.getElementById('para').style.color = color;
+  document.getElementById("bulb").style.backgroundColor = color;
 }
