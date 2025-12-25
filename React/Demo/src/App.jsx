4414b73200326943438f3b80f7f547b4964c8f16
sanjana-vi20 <vishwakarmasunita963@gmail.com>
2025-12-25 14:47:47 +0530

    feat: initialize React demo project with Vite
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created main application component (App.jsx) with basic structure and a console log.
    - Added React logo SVG in the assets directory.
    - Defined basic CSS styles for the header.
    - Set up main entry point (main.jsx) for rendering the application.
    - Configured Vite with React plugin in vite.config.js.

---
+++ React/Demo/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+function App() {
+  let a = 9;
+  let b = 1;
+  console.log(a + b);
+
+  return (
+    <>
+      <h1>Hello word</h1>
+      <div>
+        <h1 id="head">This is my React App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
