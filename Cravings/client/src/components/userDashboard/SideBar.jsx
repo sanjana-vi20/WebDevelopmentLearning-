cd4895dbffb8a10bae0fe14c8c948bea6aabe86f
sanjana-vi20 <vishwakarmasunita963@gmail.com>
2026-01-17 18:29:59 +0530

    feat: enhance sidebar buttons with active state styling and improved hover effects

--- Cravings/client/src/components/userDashboard/SideBar.jsx
+++ Cravings/client/src/components/userDashboard/SideBar.jsx
@@ -5,26 +5,71 @@ import { TbTransactionPound } from "react-icons/tb";
 import { MdOutlineHelpOutline } from "react-icons/md";
 import { CiGrid41 } from "react-icons/ci";
 
-const SideBar = ({active , setActive}) => {
-
-    
+const SideBar = ({ active, setActive }) => {
   return (
     <>
       <div className=" p-3">
-        <div className="text-xl font-bold">
-            User Dashboard
-        </div>
+        <div className="text-xl font-bold">User Dashboard</div>
         <hr />
 
         <div className="flex flex-col gap-3 p-3">
-            <button className="flex items-center gap-2" onClick={()=>setActive('overview')}><CiGrid41 />Overview</button>
-           <button className="flex items-center gap-2" onClick={()=>setActive('profile')}><CgProfile />Profiles</button>
-            <button className="flex items-center gap-2" onClick={()=>setActive('order')}><IoCart />Orders</button>
-            <button className="flex items-center gap-2" onClick={()=>setActive('transaction')}><TbTransactionPound />Transactions</button>
-            <button className="flex items-center gap-2" onClick={()=>setActive('helpdesk')}><MdOutlineHelpOutline />Help Desk</button>
+          <button
+            className={`flex items-center gap-2 p-2 rounded ${
+              active === "overview"
+                ? "bg-(--bg-light)"
+                : " hover:bg-amber-50/70 hover:scale-105"
+            }`}
+            onClick={() => setActive("overview")}
+          >
+            <CiGrid41 />
+            Overview
+          </button>
+          <button
+            className={`flex items-center gap-2 p-2 rounded ${
+              active === "profile"
+                ? "bg-(--bg-light)"
+                : " hover:bg-amber-50/70 hover:scale-105"
+            }`}
+            onClick={() => setActive("profile")}
+          >
+            <CgProfile />
+            Profiles
+          </button>
+          <button
+           className={`flex items-center gap-2 p-2 rounded ${
+              active === "order"
+                ? "bg-(--bg-light)"
+                : " hover:bg-amber-50/70 hover:scale-105"
+            }`}
+            onClick={() => setActive("order")}
+          >
+            <IoCart />
+            Orders
+          </button>
+          <button
+           className={`flex items-center gap-2 p-2 rounded ${
+              active === "transaction"
+                ? "bg-(--bg-light)"
+                : " hover:bg-amber-50/70 hover:scale-105"
+            }`}
+            onClick={() => setActive("transaction")}
+          >
+            <TbTransactionPound />
+            Transactions
+          </button>
+          <button
+            className={`flex items-center gap-2 p-2 rounded ${
+              active === "helpdesk"
+                ? "bg-(--bg-light)"
+                : " hover:bg-amber-50/60 hover:scale-105"
+            }`}
+            onClick={() => setActive("helpdesk")}
+          >
+            <MdOutlineHelpOutline />
+            Help Desk
+          </button>
         </div>
       </div>
-      
     </>
   );
 };
